e12a8168f2e4fd78280d2785d0d37ba862578a49
jomalusa <102612175+jomalusa@users.noreply.github.com>
2022-03-29 18:23:58 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Probar en GitHub</title>
+</head>
+<style>
+h1{
+    color: red;
+    font-family:cursive;
+    font-weight: bold;
+    font-style: italic;
+}
+h2{
+    color: blue;
+    font-family:cursive;
+    font-weight: bold;
+    font-style: italic;
+}
+</style>
+<body>
+    <h1>Este es un Hola Mundo</h1>
+    <h2 class="rojo">Este es otro</h2>
+    <?php
+    echo "Hola Mundito PHP";
+    ?>
+
+</body>
+</html>
